38e3c5a500d6b8e9161c37406a588138fd60b868
takuchalle <me@takuchalle.dev>
2023-02-27 13:35:43 +0900

    Initial Commit

---
+++ swiftui-samples/swiftui_samplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_samplesApp.swift
+//  swiftui-samples
+//
+//  Created by Takuya OHASHI on 2023/02/27.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_samplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
